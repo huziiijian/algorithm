fc80b993ad7be7dacfcb032cb3025a91da3ba442
huziiijian <772964145@qq.com>
2020-12-06 22:12:55 +0800

    合并有序链表

--- src/pages/List/model/index.ts
+++ src/pages/List/model/index.ts
@@ -6,21 +6,21 @@ import LifeCycle, { LifeCycleProps } from "src/utils/VM/lifeCycle";
 interface Params {}
 interface Query {}
 const list1 = {
-    val: 2,
+    val: 1,
     next: {
-        val: 4,
+        val: 2,
         next: {
-            val: 3,
+            val: 7,
             next: null,
         },
     },
 };
 const list2 = {
-    val: 5,
+    val: 4,
     next: {
-        val: 6,
+        val: 5,
         next: {
-            val: 4,
+            val: 6,
             next: null,
         },
     },
@@ -38,7 +38,8 @@ class PageModel extends LifeCycle<Params, Query> {
     constructor(props: LifeCycleProps<Params, Query>) {
         super(props);
         // this.addTwoNumbers(list1, list2)
-        this.reverseList(list1);
+        //   this.reverseList(list1);
+        console.log(this.mergeTwoLists1(list1, list2));
     }
     @observable showLoading = true;
 
@@ -79,6 +80,39 @@ class PageModel extends LifeCycle<Params, Query> {
             head = current; // head降级
         }
         console.log(tail);
+    };
+
+    //  合并有序链表
+    mergeTwoLists1 = (list1: any, list2: any) => {
+        if (list1 === null) { // 防止特殊情况
+            return list2; 
+        }
+        if (list2 === null) { // 防止特殊情况
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = this.mergeTwoLists1(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = this.mergeTwoLists2(list1, list2.next);
+            return list2;
+        }
+    };
+    mergeTwoLists2 = (l1: any, l2: any) => {
+        var prevHead = new ListNode(-1);
+        var prevNode = prevHead; // 指针
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prevNode.next = l1;
+                l1 = l1.next;
+            } else {
+                prevNode.next = l2;
+                l2 = l2.next;
+            }
+            prevNode = prevNode.next;
+        }
+        prevNode.next = l1 ? l1 : l2; // 指向剩余的l1,l2
+        return prevHead.next;
     };
 }
 
--- src/pages/List/view/index.tsx
+++ src/pages/List/view/index.tsx
@@ -1,25 +1,33 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import { observer } from "mobx-react";
-import style from '../style/index.less';
+import style from "../style/index.less";
 
-
-
-import Model from '../model'
+import Model from "../model";
 interface Props {
-    store: Model
+    store: Model;
 }
 
-const View:FC<Props> = observer((props) => {
-
+const View: FC<Props> = observer((props) => {
     return (
-      <div>
-         <h1>链表类算法题</h1>
-         <a href='https://leetcode-cn.com/problems/reverse-linked-list/solution/206-fan-zhuan-lian-biao-duo-chong-shi-xian-duo-cho/'>翻转单链表</a>
-         <br/><br/>
-         <a href='https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode-solution/'>两数相加</a>
-         <br/><br/>
-      </div>
+        <div>
+            <h1>链表类算法题</h1>
+            <a href="https://leetcode-cn.com/problems/reverse-linked-list/solution/206-fan-zhuan-lian-biao-duo-chong-shi-xian-duo-cho/">
+                翻转单链表
+            </a>
+            <br />
+            <br />
+            <a href="https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode-solution/">
+                两数相加
+            </a>
+            <br />
+            <br />
+            <a href="https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/21-he-bing-liang-ge-you-xu-lian-biao-by-alexer-6-2/">
+                合并有序链表
+            </a>
+            <br />
+            <br />
+        </div>
     );
-})
+});
 
 export default View;
