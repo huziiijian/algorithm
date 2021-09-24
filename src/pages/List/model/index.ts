02e4c2ed51b42e92b107d9090b14c2192363ce49
zijian hu <zijianhu@zijiandeMacBook-Pro.local>
2021-09-24 18:34:49 +0800

    list

--- src/pages/List/model/index.ts
+++ src/pages/List/model/index.ts
@@ -1,17 +1,35 @@
-import { observable, computed, action } from "mobx";
-import { history, qs } from "src/utils";
-import Service from "../service";
-
 import LifeCycle, { LifeCycleProps } from "src/utils/VM/lifeCycle";
 interface Params {}
 interface Query {}
 const list1 = {
-    val: 1,
-    next: {}
+    val: 9,
+    next: {
+        val: 8,
+        next: {
+            val: 7,
+            next: {},
+        },
+    },
 };
 const list2 = {
     val: 4,
-    next: {}
+    next: null,
+};
+const list3 = {
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: {
+                val: 4,
+                next: {
+                    val: 5,
+                    next: null,
+                },
+            },
+        },
+    },
 };
 class ListNode {
     val: any;
@@ -25,16 +43,31 @@ class ListNode {
 class PageModel extends LifeCycle<Params, Query> {
     constructor(props: LifeCycleProps<Params, Query>) {
         super(props);
-        // this.addTwoNumbers(list1, list2)
-        //   this.reverseList(list1);
-        console.log(this.mergeTwoLists1(list1, list2));
+        // console.log(this.reverseList(list3));
+        console.log(this.addTwoNumbers(list1, list2));
+        // console.log(this.mergeTwoLists1(list1, list2));
     }
-    @observable showLoading = true;
+
+    // 翻转单链表
+    reverseList = (list1: any) => {
+        if (!list1 || !list1.next) return list1;
+        let head = list1,
+            tail: any = {};
+        // 注意链表的末节点是null
+        while (head) {
+            // 1,4步用来遍历，2,3步用来赋值
+            const current = head.next; // 中间值保存当前节点指针指向
+            head.next = tail; // 使当前节点指针指向上次的tail
+            tail = head; // 新tail获得当前head值，上一步中已将指针指向老tail
+            head = current; // head降级
+        }
+        return tail;
+    };
 
     // 两数相加
     addTwoNumbers = (list1: any, list2: any) => {
         let head: any = null,
-            tail: any = null; // head作为输出，tail作为过程计算值
+            curr: any = null; // head作为输出，curr作为过程计算值
         let carry = 0; // 代表进位值
         while (list1 || list2) {
             // 通过指针list,list2判断是否到达循环终点
@@ -43,45 +76,34 @@ class PageModel extends LifeCycle<Params, Query> {
             const sum = n1 + n2 + carry; // 每位总值等于同级数值加上一位的进位值
             if (!head) {
                 // 首次遍历，创造根节点
-                head = tail = new ListNode(sum % 10);
+                head = curr = new ListNode(sum % 10);
             } else {
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next; // 指针继续指向下一层
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next; // 指针继续指向下一层
             }
             carry = Math.floor(sum / 10);
             list1 && (list1 = list1.next);
             list2 && (list2 = list2.next);
         }
-        carry && (tail.next = new ListNode(carry)); // 考虑到最后位相加依然进位的情况
-        console.log(head);
-    };
-
-    // 翻转单链表
-    reverseList = (list1: any) => {
-        if (!list1 || !list1.next) return list1;
-        let head = list1,
-            tail: any = null;
-        while (head) { // 1,4步用来遍历，2,3步用来赋值
-            const current = head.next; // 保存当前节点的指针指向
-            head.next = tail; // 使当前节点指针指向老tail
-            tail = head; // 新tail获得当前head值，并能将指针指向老tail，不会丢失之前的赋值
-            head = current; // head降级
-        }
-        console.log(tail);
+        carry && (curr.next = new ListNode(carry)); // 考虑到最后位相加依然进位的情况
+        return head;
     };
 
     //  合并有序链表
     mergeTwoLists1 = (list1: any, list2: any) => {
-        if (list1 === null) { // 防止特殊情况，若有一者为空则返回非空链表
-            return list2; 
+        if (list1 === null) {
+            // 防止特殊情况，若有一者为空则返回非空链表
+            return list2;
         }
-        if (list2 === null) { // 防止特殊情况
+        if (list2 === null) {
+            // 防止特殊情况
             return list1;
         }
         if (list1.val < list2.val) {
             list1.next = this.mergeTwoLists1(list1.next, list2);
             return list1;
-        } else { // 注意上面的非空判断并不是常规的出口，这里的else为list.next在非null情况下，最后所有情况的出口
+        } else {
+            // 注意上面的非空判断并不是常规的出口，这里的else为list.next在非null情况下，最后所有情况的出口
             list2.next = this.mergeTwoLists2(list1, list2.next);
             return list2;
         }
